982eb66ac0fff8ccb8367d9ed1e5ca43088356e3
Rafael Moreira Ramos de Rezende <100146657+UPraggy@users.noreply.github.com>
2022-05-27 12:41:33 -0300

    Add files via upload

---
+++ utils/INTERFACE_FUNCIONALITIES/resources.py
@@ -0,0 +1,21 @@
+from utils import template
+from PyQt5.QtWidgets import QTreeWidgetItem
+import datetime as dtime 
+from dateutil import rrule
+import matplotlib.pyplot as plt
+from pandas import concat as pd_concat
+
+c = template.Client()
+p = template.Product()
+o = template.Order()
+
+
+
+def switch_page(stacked, page):
+        stacked.setCurrentWidget(page)
+
+                                                ################################################
+                                                ################################################
+                                                ##########CLASSES FOR CLIENT OPERATIONS#########
+       ata]
+      
